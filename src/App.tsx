2a90c4864ce0a4b67d554f3f88f515bd7c2c1dcd
Bethany Burrows <jedcburrows@gmail.com>
2024-09-02 10:26:43 +0100

    Setup components for the number display & the num pad.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import CalcBody from "./Components/CalcBody/CalcBody";
 import Header from "./Components/Header/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <CalcBody />
     </div>
   );
 };
---
+++ src/Components/NumPad/NumPad.tsx
@@ -0,0 +1,11 @@
+import "./NumPad.scss";
+
+const NumPad = () => {
+  return (
+    <div className="num-pad">
+      <button>0</button>
+    </div>
+  );
+};
+
+export default NumPad;
