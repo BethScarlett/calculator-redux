e0f59c9a699a4373b2ea12c1fdb9ef1b3806ab08
Bethany Burrows <jedcburrows@gmail.com>
2024-09-02 11:02:34 +0100

    Replaced button component with a better named 'NumButton' component, and setup required functionality & display properties.

--- src/Components/NumPad/NumPad.tsx
+++ src/Components/NumPad/NumPad.tsx
@@ -1,9 +1,32 @@
+import NumButton from "../NumButton/NumButton";
 import "./NumPad.scss";
 
 const NumPad = () => {
+  const placeholderFunc = () => {
+    console.log("Function");
+  };
+
   return (
     <div className="num-pad">
-      <button>0</button>
+      <NumButton value="C" action={placeholderFunc} />
+      <NumButton value="+-" action={placeholderFunc} />
+      <NumButton value="%" action={placeholderFunc} />
+      <NumButton value="/" action={placeholderFunc} />
+      <NumButton value="7" action={placeholderFunc} />
+      <NumButton value="8" action={placeholderFunc} />
+      <NumButton value="9" action={placeholderFunc} />
+      <NumButton value="*" action={placeholderFunc} />
+      <NumButton value="4" action={placeholderFunc} />
+      <NumButton value="5" action={placeholderFunc} />
+      <NumButton value="6" action={placeholderFunc} />
+      <NumButton value="-" action={placeholderFunc} />
+      <NumButton value="1" action={placeholderFunc} />
+      <NumButton value="2" action={placeholderFunc} />
+      <NumButton value="3" action={placeholderFunc} />
+      <NumButton value="+" action={placeholderFunc} />
+      <NumButton value="0" action={placeholderFunc} />
+      <NumButton value="." action={placeholderFunc} />
+      <NumButton value="=" action={placeholderFunc} />
     </div>
   );
 };
