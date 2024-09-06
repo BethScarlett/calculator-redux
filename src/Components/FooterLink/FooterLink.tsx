8a2bdf1f97b3a460ae7bdc6c8b82099d10ec8ebf
Bethany Burrows <jedcburrows@gmail.com>
2024-09-06 11:29:06 +0100

    Added accurate images to footer links, and altered component to take in & use a destination url.

--- src/Components/FooterLink/FooterLink.tsx
+++ src/Components/FooterLink/FooterLink.tsx
@@ -4,13 +4,16 @@ type FooterLinkProps = {
   labelText: string;
   imgSrc: string;
   imgAltDest: string;
+  imgLink: string;
 };
 
-const FooterLink = ({ labelText, imgSrc, imgAltDest }: FooterLinkProps) => {
+const FooterLink = ({ labelText, imgSrc, imgAltDest, imgLink }: FooterLinkProps) => {
   return (
     <div className="footer-link">
       <label>{labelText}</label>
-      <img src={imgSrc} alt={`Link to ${imgAltDest}`} />
+      <a href={imgLink} target="_blank">
+        <img src={imgSrc} alt={`Link to ${imgAltDest}`} />
+      </a>
     </div>
   );
 };
